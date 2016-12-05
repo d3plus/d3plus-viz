2738444234b19e0721d145504b6ea652d1302db9
Dave Landry <landry.dave@gmail.com>
2016-12-04 22:19:59 -0500

    sets tooltip footer text based on locale

--- src/on/mouseenter.legend.js
+++ src/on/mouseenter.legend.js
@@ -1,5 +1,7 @@
 import {mouse, select} from "d3-selection";
 
+import {locale} from "d3plus-common";
+
 /**
     @desc On mouseenter event for all legend shapes in a Viz.
     @param {Object} *d* The data object being interacted with.
@@ -12,7 +14,7 @@ export default function(d) {
     const depth = this._drawDepth < this._groupBy.length - 1;
     this._select.style("cursor", depth ? "pointer" : "auto");
     this._tooltipClass.data([d])
-      .footer(depth ? "Click to Expand" : "")
+      .footer(depth ?  locale.t("Click to Expand", {lng: this._locale}) : "")
       .title(this._legendClass.label())
       .translate(mouse(select("html").node()))
       .config(this._tooltipConfig)
