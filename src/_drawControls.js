def524ef261202127fd3a1be9de096af1cd84e89
Dave Landry <landry.dave@gmail.com>
2017-03-24 16:37:57 -0400

    controls now support "position" key

--- src/_drawControls.js
+++ src/_drawControls.js
@@ -10,80 +10,92 @@ const formTypes = {Radio, Select};
 */
 export default function() {
 
-  const condition = this._controls && this._controls.length;
   const that = this;
-  const transform = {
-    height: this._height - this._margin.top - this._margin.bottom,
-    width: this._width - this._margin.left - this._margin.right,
-    x: this._margin.left,
-    y: this._margin.top
-  };
 
-  const foreign = elem("foreignObject.d3plus-viz-controls", {
-    condition,
-    enter: Object.assign({opacity: 0}, transform),
-    exit: Object.assign({opacity: 0}, transform),
-    parent: this._select,
-    transition: this._transition,
-    update: Object.assign({opacity: 1}, transform)
-  });
+  const areas = ["left", "right", "top", "bottom"];
+  for (let a = 0; a < areas.length; a++) {
+    const area = areas[a];
+    const controls = (this._controls || []).filter(c => !c.position && area === "bottom" || c.position === area);
 
-  let container = foreign.selectAll("div.d3plus-viz-controls-container")
-    .data([null]);
+    const transform = {
+      height: this._height - this._margin.top - this._margin.bottom,
+      width: this._width - this._margin.left - this._margin.right,
+      x: this._margin.left,
+      y: this._margin.top
+    };
 
-  container = container.enter().append("xhtml:div")
-      .attr("class", "d3plus-viz-controls-container")
-      .style("margin-top", `${transform.height}px`)
-    .merge(container);
+    const foreign = elem(`foreignObject.d3plus-viz-controls-${area}`, {
+      condition: controls.length,
+      enter: Object.assign({opacity: 0}, transform),
+      exit: Object.assign({opacity: 0}, transform),
+      parent: this._select,
+      transition: this._transition,
+      update: Object.assign({opacity: 1}, transform)
+    });
 
-  if (condition) {
+    let container = foreign.selectAll("div.d3plus-viz-controls-container")
+      .data([null]);
 
-    for (let i = 0; i < this._controls.length; i++) {
+    container = container.enter().append("xhtml:div")
+        .attr("class", "d3plus-viz-controls-container")
+        .style("margin-top", area === "bottom" ? `${transform.height}px` : 0)
+        .style("margin-left", area === "right" ? `${transform.width}px` : 0)
+      .merge(container);
 
-      const control = this._controls[i];
+    if (controls.length) {
 
-      const on = {};
-      if (control.on) {
-        for (const event in control.on) {
-          if ({}.hasOwnProperty.call(control.on, event)) {
-            on[event] = function() {
-              control.on[event].bind(that)(this.value);
-            };
+      for (let i = 0; i < controls.length; i++) {
+
+        const control = controls[i];
+
+        const on = {};
+        if (control.on) {
+          for (const event in control.on) {
+            if ({}.hasOwnProperty.call(control.on, event)) {
+              on[event] = function() {
+                control.on[event].bind(that)(this.value);
+              };
+            }
+          }
+
+        }
+
+        const id = control.label || `${area}-${i}`;
+        if (!this._controlCache[id]) {
+          const type = control.type && formTypes[control.type] ? control.type : "Select";
+          this._controlCache[id] = new formTypes[type]().container(container.node());
+          if (control.checked) {
+            this._controlCache[id].checked(control.checked);
+            delete control.checked;
+          }
+          if (control.selected) {
+            this._controlCache[id].selected(control.selected);
+            delete control.selected;
           }
         }
 
+        this._controlCache[id]
+          .config(control)
+          .config({on})
+          .config(this._controlConfig)
+          .render();
+
       }
 
-      const id = control.label || i;
-      if (!this._controlCache[id]) {
-        const type = control.type && formTypes[control.type] ? control.type : "Select";
-        this._controlCache[id] = new formTypes[type]().container(container.node());
-        if (control.checked) {
-          this._controlCache[id].checked(control.checked);
-          delete control.checked;
-        }
-        if (control.selected) {
-          this._controlCache[id].selected(control.selected);
-          delete control.selected;
-        }
-      }
+      container
+          .style("display", ["top", "bottom"].includes(area) ? "block" : "inline-block")
+          .style("text-align", ["top", "bottom"].includes(area) ? "center" : area);
 
-      this._controlCache[id]
-        .config(control)
-        .config({on})
-        .config(this._controlConfig)
-        .render();
+      const bounds = container.node().getBoundingClientRect();
+
+      container
+        .transition(this._transition)
+          .style("margin-top", area === "bottom" ? `${transform.height - bounds.height}px` : 0)
+          .style("margin-left", area === "right" ? `${transform.width - bounds.width}px` : 0);
+
+      this._margin[area] += ["top", "bottom"].includes(area) ? bounds.height : bounds.width;
 
     }
-
-    const bounds = container.node().getBoundingClientRect();
-
-    container
-        .style("text-align", "center")
-      .transition(this._transition)
-        .style("margin-top", `${transform.height - bounds.height}px`);
-
-    this._margin.bottom += bounds.height;
 
   }
 
