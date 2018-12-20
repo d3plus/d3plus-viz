726f6cf285189c6cc9385d6301a8fce12c29ee26
cnavarreteliz <cnavarreteliz@gmail.com>
2018-12-20 12:17:37 -0300

    fixes weird behavior switching between colorScale and non-colorScale

--- src/_drawColorScale.js
+++ src/_drawColorScale.js
@@ -9,55 +9,53 @@ export default function() {
 
   const data = this._data;
 
-  if (this._colorScale && data) {
+  const position = this._colorScalePosition || "bottom";
+  const wide = ["top", "bottom"].includes(position);
 
-    const position = this._colorScalePosition || "bottom";
-    const wide = ["top", "bottom"].includes(position);
+  const transform = {
+    opacity: this._colorScalePosition ? 1 : 0,
+    transform: `translate(${wide ? this._margin.left + this._padding.left : this._margin.left}, ${wide ? this._margin.top : this._margin.top + this._padding.top})`
+  };
 
-    const transform = {
-      opacity: this._colorScalePosition ? 1 : 0,
-      transform: `translate(${wide ? this._margin.left + this._padding.left : this._margin.left}, ${wide ? this._margin.top : this._margin.top + this._padding.top})`
-    };
+  const showColorScale = this._colorScale && data && data.length > 1;
 
-    const showColorScale = this._colorScale && data && data.length > 1;
+  const scaleGroup = elem("g.d3plus-viz-colorScale", {
+    condition: showColorScale && !this._colorScaleConfig.select,
+    enter: transform,
+    parent: this._select,
+    transition: this._transition,
+    update: transform
+  }).node();
 
-    const scaleGroup = elem("g.d3plus-viz-colorScale", {
-      condition: showColorScale && !this._colorScaleConfig.select,
-      enter: transform,
-      parent: this._select,
-      transition: this._transition,
-      update: transform
-    }).node();
-
+  if (showColorScale) {
     const scaleData = data.filter((d, i) => {
       const c = this._colorScale(d, i);
       return c !== undefined && c !== null;
     });
 
-    if (showColorScale) {
-      this._colorScaleClass
-        .align({bottom: "end", left: "start", right: "end", top: "start"}[position] || "bottom")
-        .duration(this._duration)
-        .data(scaleData)
-        .height(wide ? this._height - (this._margin.bottom + this._margin.top) : this._height - (this._margin.bottom + this._margin.top + this._padding.bottom + this._padding.top))
-        .orient(position)
-        .select(scaleGroup)
-        .value(this._colorScale)
-        .width(wide ? this._width - (this._margin.left + this._margin.right + this._padding.left + this._padding.right) : this._width - (this._margin.left + this._margin.right))
-        .config(this._colorScaleConfig)
-        .render();
+    this._colorScaleClass
+      .align({bottom: "end", left: "start", right: "end", top: "start"}[position] || "bottom")
+      .duration(this._duration)
+      .data(scaleData)
+      .height(wide ? this._height - (this._margin.bottom + this._margin.top) : this._height - (this._margin.bottom + this._margin.top + this._padding.bottom + this._padding.top))
+      .orient(position)
+      .select(scaleGroup)
+      .value(this._colorScale)
+      .width(wide ? this._width - (this._margin.left + this._margin.right + this._padding.left + this._padding.right) : this._width - (this._margin.left + this._margin.right))
+      .config(this._colorScaleConfig)
+      .render();
 
-      const scaleBounds = this._colorScaleClass.outerBounds();
-      if (this._colorScalePosition && !this._colorScaleConfig.select && scaleBounds.height) {
-        if (wide) this._margin[position] += scaleBounds.height + this._legendClass.padding() * 2;
-        else this._margin[position] += scaleBounds.width + this._legendClass.padding() * 2;
-      }
-
-    }
-    else {
-      this._colorScaleClass.config(this._colorScaleConfig);
+    const scaleBounds = this._colorScaleClass.outerBounds();
+    if (this._colorScalePosition && !this._colorScaleConfig.select && scaleBounds.height) {
+      if (wide) this._margin[position] += scaleBounds.height + this._legendClass.padding() * 2;
+      else this._margin[position] += scaleBounds.width + this._legendClass.padding() * 2;
     }
 
   }
+  else {
+    this._colorScaleClass.config(this._colorScaleConfig);
+  }
+
+  
 
 }
