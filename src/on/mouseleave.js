c22e2358515b416529ac2b69e3fe2cc267c302e3
Dave Landry <landry.dave@gmail.com>
2019-05-02 16:58:45 -0400

    fixes tooltip removal on multi-temporal shapes (ie. Lines)

--- src/on/mouseleave.js
+++ src/on/mouseleave.js
@@ -10,7 +10,7 @@ export default function(d, i) {
     if (this._shapeConfig.hoverOpacity !== 1 && this._hover ? this._hover(d, i) : true) {
       this.hover(false);
     }
-    if (this._tooltip && this._tooltipClass.data()[0] === d) this._tooltipClass.data([]).render();
+    if (this._tooltip && this._id(this._tooltipClass.data()[0]) === this._id(d)) this._tooltipClass.data([]).render();
   }, 50);
 
   this._select.style("cursor", "auto");
